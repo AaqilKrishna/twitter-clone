d2cc789437746dc4a8b7dfffd7fa596599ef91fa
Aaqil Raj Krishna <aaqilrk@gmail.com>
2023-03-15 00:55:05 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/socialdev/twitter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.socialdev.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6410c1003982636203b7';
+  static const String projectId = '6410b7f03cc81c78eb06';
+  static const String endPoint = 'http://localhost:80/v1';
+}
