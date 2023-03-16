a21c20b64c98696f0ef709e339451cef486d49a2
Aaqil Raj Krishna <aaqilrk@gmail.com>
2023-03-17 00:14:43 +0530

    add login UI

---
+++ lib/common/rounded_small_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../theme/palette.dart';
+
+class RoundedSmallButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String label;
+  final Color backgroundColor;
+  final Color textColor;
+
+  const RoundedSmallButton({
+    super.key,
+    required this.onTap,
+    required this.label,
+    this.backgroundColor = Palette.whiteColor,
+    this.textColor = Palette.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      label: Text(
+        label,
+        style: TextStyle(
+          color: textColor,
+          fontSize: 16,
+        ),
+      ),
+      backgroundColor: backgroundColor,
+      labelPadding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 5,
+      ),
+    );
+  }
+}
