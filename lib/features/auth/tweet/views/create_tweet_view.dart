0c9591f6e4764c4ffd620a242903b9ed7dddcece
vardhinialuru <vardhinisharma08@gmail.com>
2023-05-10 01:29:55 +0530

    Add tweet screen UI cont

--- lib/features/auth/tweet/views/create_tweet_view.dart
+++ lib/features/auth/tweet/views/create_tweet_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:twitter_clone/common/loading_page.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import '../../../../common/rounded_small_button.dart';
+import '../../../../constants/assets_constants.dart';
 import '../../../../theme/palette.dart';
 
 class CreateTweetScreen extends ConsumerStatefulWidget {
@@ -10,13 +12,19 @@ class CreateTweetScreen extends ConsumerStatefulWidget {
         builder: (context) => const CreateTweetScreen(),
       );
   const CreateTweetScreen({super.key});
-
   @override
   ConsumerState<ConsumerStatefulWidget> createState() =>
       _CreateTweetScreenState();
 }
 
 class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
+  final tweetTextController = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    tweetTextController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDetailsProvider).value;
@@ -48,11 +56,66 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
                         backgroundImage: NetworkImage(currentUser.profilePic),
                         radius: 30,
                       ),
+                      const SizedBox(width: 15),
+                      Expanded(
+                        child: TextField(
+                          controller: tweetTextController,
+                          style: const TextStyle(
+                            fontSize: 22,
+                          ),
+                          decoration: const InputDecoration(
+                            hintText: "What's happening?",
+                            hintStyle: TextStyle(
+                              color: Palette.greyColor,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                          maxLines: null,
+                        ),
+                      )
                     ],
                   ),
                 ]),
               ),
             ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.only(bottom: 10),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Palette.greyColor,
+              width: 0.3,
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.galleryIcon),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.gifIcon),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.emojiIcon),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
