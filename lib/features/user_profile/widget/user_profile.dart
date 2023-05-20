b36b85e6ef69ea13b8a8ac542b1f6694737c3b3b
Snehanjali <sneha.g.anjali@gmail.com>
2023-05-20 22:26:41 +0530

    Following other users

--- lib/features/user_profile/widget/user_profile.dart
+++ lib/features/user_profile/widget/user_profile.dart
@@ -37,8 +37,8 @@ class UserProfile extends ConsumerWidget {
                                 color: Palette.blueColor,
                               )
                             : Image.network(
-                              user.bannerPic, 
-                              fit: BoxFit.fitWidth,
+                                user.bannerPic,
+                                fit: BoxFit.fitWidth,
                               ),
                       ),
                       Positioned(
@@ -55,6 +55,14 @@ class UserProfile extends ConsumerWidget {
                           onPressed: () {
                             if (currentUser.uid == user.uid) {
                               Navigator.push(context, EditProfileView.route());
+                            } else {
+                              ref
+                                  .read(userProfileControllerProvider.notifier)
+                                  .followUser(
+                                    user: user,
+                                    context: context,
+                                    currentUser: currentUser,
+                                  );
                             }
                           },
                           style: ElevatedButton.styleFrom(
@@ -67,7 +75,9 @@ class UserProfile extends ConsumerWidget {
                           child: Text(
                             currentUser.uid == user.uid
                                 ? 'Edit profile'
-                                : 'Follow',
+                                : currentUser.following.contains(user.uid) 
+                                    ? 'Unfollow'
+                                    : 'Follow',
                             style: const TextStyle(
                               color: Palette.whiteColor,
                             ),
